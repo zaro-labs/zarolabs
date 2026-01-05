c336a97922ffa42340597835bbfccb46ef93aa10
Xlib <aman9893089064@gmail.com>
2026-01-06 04:36:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,11 @@ export const metadata = {
   description: 'Web, Mobile, AI & Mixed Reality specialists.',
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body style={{ margin: 0, padding: 0 }}>
